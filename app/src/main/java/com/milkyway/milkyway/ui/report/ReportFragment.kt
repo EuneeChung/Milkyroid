3b21725be256eac51e22d071b1c45d9ed3b03ee2
doodung <leesujung0917@gmail.com>
2021-01-05 06:48:06 +0900

    [feat] #14  Add reportfragmnet tab layout

--- app/src/main/java/com/milkyway/milkyway/ui/report/ReportFragment.kt
+++ app/src/main/java/com/milkyway/milkyway/ui/report/ReportFragment.kt
@@ -5,14 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.milkyway.milkyway.R
+import com.milkyway.milkyway.ui.report.adapter.ReportViewPagerAdapter
 
 class ReportFragment : Fragment() {
+    private lateinit var viewpagerAdapter: ReportViewPagerAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_report, container, false)
 
-        return inflater.inflate(R.layout.fragment_report, container, false)
+        viewpagerAdapter= ReportViewPagerAdapter(childFragmentManager)
+        viewpagerAdapter.fragments= listOf(
+            CafeReportFragment(),
+            MyReportFragment()
+        )
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val vptab = getView()?.findViewById<ViewPager>(R.id.vp_report_tab)
+        val tab = getView()?.findViewById<TabLayout>(R.id.tab_report)
+        vptab?.adapter=viewpagerAdapter
+        tab?.setupWithViewPager(vptab)
+        tab?.apply {
+            getTabAt(0)?.text="카페 제보"
+            getTabAt(1)?.text="나의 제보"
+        }
     }
 }
