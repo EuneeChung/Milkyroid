30fc40198cc68be4724fed2172366c6d31d1d74b
kym1924 <kym1924@naver.com>
2021-01-13 00:11:59 +0900

    [feature] #125 Current Location UniverseFragment

--- app/src/main/java/com/milkyway/milkyway/ui/universe/UniverseFragment.kt
+++ app/src/main/java/com/milkyway/milkyway/ui/universe/UniverseFragment.kt
@@ -13,9 +13,12 @@ import com.milkyway.milkyway.R
 import com.milkyway.milkyway.databinding.FragmentUniverseBinding
 import com.milkyway.milkyway.util.DataStore
 import com.milkyway.milkyway.util.UniverseMarkerDrawer
+import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.OverlayImage
+import com.naver.maps.map.util.FusedLocationSource
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
@@ -79,6 +82,8 @@ class UniverseFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(p0: NaverMap) {
         setUiSetting(p0)
+        setLocation(p0)
+        setCurrentLocationIcon(p0)
         setLightness(p0)
         setMapClickListener(p0)
         drawMarkers(p0)
@@ -94,6 +99,31 @@ class UniverseFragment : Fragment(), OnMapReadyCallback {
 
     private fun setLightness(p0: NaverMap) {
         p0.lightness = -0.7f
+    }
+
+    private fun setLocation(p0 : NaverMap) {
+        val locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        p0.locationSource = locationSource
+    }
+
+    private fun setCurrentLocationIcon(p0 : NaverMap) {
+        val locationOverlay = p0.locationOverlay
+        locationOverlay.isVisible = true
+        locationOverlay.icon = OverlayImage.fromResource(R.drawable.ic_current_location_universe)
+        locationOverlay.circleRadius = 0
+
+        universeViewModel.compass.observe(this, Observer { compass->
+            compass?.let {
+                if(compass) {
+                    p0.locationTrackingMode = LocationTrackingMode.Face
+                    locationOverlay.subIcon = OverlayImage.fromResource(R.drawable.ic_universe_location_face)
+                }
+                else {
+                    p0.locationTrackingMode = LocationTrackingMode.NoFollow
+                    locationOverlay.subIcon = OverlayImage.fromResource(R.drawable.ic_universe_location_no_follow)
+                }
+            }
+        })
     }
 
     private fun setMapClickListener(p0 : NaverMap) {
@@ -126,8 +156,7 @@ class UniverseFragment : Fragment(), OnMapReadyCallback {
         })
     }
 
-    override fun onResume() {
-        super.onResume()
-        universeViewModel.setMapClick()
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
